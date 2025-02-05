e27d32b3e8d824b148a585d51d595b33454ce46a
Bibesh10 <bibeshshrestha@gmail.com>
2025-02-05 13:20:18 +0545

    made form page and login page

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Page</title>
+</head>
+<body>
+<form >
+    <input type="email" name="email" placeholder="Email">
+
+    <input type="password" name="password" placeholder="Password">
+    
+    <button type="submit">Login</button>
+</form>
+</body>
+</html>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Page</title>
+</head>
+<body>
+<form>
+  <label>User
+    <br>
+    <input type="Username" name="username" placeholder="Username"></label>
+    <br>
+  
+  <label>Email
+    <br>
+    <input type="email" name="email" placeholder="Email address"></label>
+    <br>
+
+  <label>Password 
+    <br>
+    <input type="password" placeholder="password"></label>
+    <br>
+
+  <label>Confirm Password 
+    <br>
+    <input type="password" placeholder="password"></label>
+    <br>
+    <button>Register</button>
+</form>
+
+
+</body>
+</html>
